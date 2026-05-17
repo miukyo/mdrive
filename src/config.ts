adc240385a1a6efbdb093576e1ed085b483f3030
miukyo <zaidaliyhijja@gmail.com>
2026-05-17 22:03:23 +0700

    feat: change port to 3002

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const config = {
   DATABASE_URL: process.env.DATABASE_URL,
-  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  PORT: parseInt(process.env.PORT || '3000', 10),
-  TELEGRAM_API_ID: parseInt(process.env.TELEGRAM_API_ID || '0', 10),
-  TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH || '',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:5173",
+  PORT: parseInt(process.env.PORT || "3002", 10),
+  TELEGRAM_API_ID: parseInt(process.env.TELEGRAM_API_ID || "0", 10),
+  TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH || "",
 };
